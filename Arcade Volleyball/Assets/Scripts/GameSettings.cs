ab32ffd39d3916f4dd25439784e8e87d20a28b39
Benjamin Haber <benhasanemail@gmail.com>
2018-10-01 13:52:50 -0400

    Added global settings class

---
+++ Arcade Volleyball/Assets/Scripts/GameSettings.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameSettings : ScriptableObject
+{
+
+	[Header("Ball Settings")] 
+	public float BallSpeed = 8;
+	public float BallGravity = -.0001f;
+	public float BallAcceleration = .001f;
+	
+	[Header("PlayerSettings")]
+	public float PlayerSpeed = 8;
+	public float PlayerJumpSpeed = 20;
+	
+	
+
+}
