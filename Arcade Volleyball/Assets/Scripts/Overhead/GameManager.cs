13b49f505b5c5c14007370dd12b244c90c6e30af
Benjamin Haber <benhasanemail@gmail.com>
2018-10-03 19:14:37 -0400

    implemented score effects using coroutines

--- Arcade Volleyball/Assets/Scripts/Overhead/GameManager.cs
+++ Arcade Volleyball/Assets/Scripts/Overhead/GameManager.cs
@@ -1,4 +1,5 @@
-﻿using TMPro;
+﻿using System.Collections;
+using TMPro;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
@@ -14,16 +15,10 @@ public class GameManager : MonoBehaviour
 	
 	private int _p1Score;
 	private int _p2Score;
-
 	private bool _paused;
-	
-	private float _serveTimer;
-	private Vector3 _nextServe;
-	private bool _effect;
 	
 	private void Start ()
 	{
-		_ball = GameObject.Find("ball").GetComponent<BallController>();
 		Menu.gameObject.SetActive(false);
 		Reset();
 	}
@@ -49,45 +44,35 @@ public class GameManager : MonoBehaviour
 
 	public void P1Score()
 	{
-		if ((_p1Score += 1) >= Settings.s.WinScore)
-			win("Player 1 wins");
-		else
-			StartScoreEffect(_ball.P1.position);
+		StartCoroutine((_p1Score += 1) >= Settings.s.WinScore ?
+			win("Player 1 wins") :
+			ScoreEffect(_ball.P1.position));
 	}
 	
 	public void P2Score()
 	{
-		if ((_p2Score += 1) >= Settings.s.WinScore)
-			win("Player 2 wins");
-		else
-			StartScoreEffect(_ball.P2.position);
+		StartCoroutine((_p2Score += 1) >= Settings.s.WinScore ? 
+			win("Player 2 wins") : 
+			ScoreEffect(_ball.P2.position));
 	}
 
-	private void StartScoreEffect(Vector3 nextServe)
+	private IEnumerator ScoreEffect(Vector3 nextServe)
 	{
 		UpdateScore();
 		_audio.HitGround();
-		_nextServe = nextServe;
-		_effect = true;
 		Time.timeScale = 0;
-		_serveTimer = Time.realtimeSinceStartup+1;
-	}
-
-	private void ScoreEffect()
-	{	
-		if (!(Time.realtimeSinceStartup > _serveTimer)) return;
-		
+		yield return new WaitForSecondsRealtime(1);
 		if (!_paused) Time.timeScale = 1;
-		_effect = false;
-		_ball.Serve(_nextServe);
-
+		_ball.Serve(nextServe);
 	}
 
-	private void win(string text)
+	private IEnumerator win(string text)
 	{
 		_audio.Win();
-		StartScoreEffect(new Vector3(0,100));
+		StartCoroutine(ScoreEffect(new Vector3(0,100)));
 		winText.text = text;
+		yield return new WaitForSecondsRealtime(5);
+		ResetGame();
 	}
 
 	private void UpdateScore()
@@ -107,7 +92,6 @@ public class GameManager : MonoBehaviour
 	{
 		if (!_ball.isStill()) instructions.gameObject.SetActive(false);
 		
-		if (_effect) ScoreEffect();
 		else if (!_paused) Time.timeScale = 1;
 			
 		if (Input.GetButtonDown("Menu")) PauseMenu();
